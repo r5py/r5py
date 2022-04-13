0ef07437d11cdb6b6c540f6bed01b5c03875eda0
Christoph Fink <1213882-christoph.fink@users.noreply.gitlab.com>
2022-04-13 12:28:41 +0300

    better memory estimation

--- src/r5py/util/jvm.py
+++ src/r5py/util/jvm.py
@@ -8,7 +8,7 @@ import jpype
 import jpype.imports
 
 from .classpath import R5_CLASSPATH
-from .memory_footprint import EIGHTY_PERCENT_OF_RAM
+from .memory_footprint import MAX_JVM_MEMORY
 
 
 __all__ = []
@@ -17,7 +17,7 @@ __all__ = []
 sys.modules['faulthandler'] = None
 
 jpype.startJVM(
-    "-Xmx{:d}G".format(EIGHTY_PERCENT_OF_RAM),
+    "-Xmx{:d}M".format(MAX_JVM_MEMORY),
     "-Xcheck:jni",
     # "--illegal-access=permit",
     # "--add-opens=java.lang/java.nio.DirectByteBuffer=ALL-UNNAMED",

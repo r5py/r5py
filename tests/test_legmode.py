d8b0a73fe947e94ba6de5a4145a05260d22ac2f0
Christoph Fink <1213882-christoph.fink@users.noreply.gitlab.com>
2022-05-02 17:39:08 +0300

    tests

---
+++ tests/test_legmode.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+import pytest  # noqa: F401
+import faulthandler
+
+import r5py
+import com.conveyal.r5
+
+
+def test_legmode():
+    walking = r5py.LegMode.WALK
+    assert walking.name == "WALK"
+    assert isinstance(walking.value, com.conveyal.r5.api.util.LegMode)

7918b291f67d716cd8159ad23f4db91b1879bc27
Henrikki Tenkanen <henrikki.tenkanen@aalto.fi>
2022-05-11 12:09:57 +0300

    Allow parsing md files for docs

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,8 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "nbsphinx",
-    "IPython.sphinxext.ipython_console_highlighting"
+    "IPython.sphinxext.ipython_console_highlighting",
+    "myst_parser"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -48,7 +49,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -66,3 +67,9 @@ sys.path.append(
         "../src/"
     )
 )
+
+# File extension mapping
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}

f380e890f6250969fece50f11c16ce1f5c00c62a
Christoph Fink <christoph+github@christophfink.com>
2022-05-12 18:00:41 +0300

    tests Christoph has written to address issue #23 (#57)
    * unit test for r5py.util.camel_to_snake_case()
    
    * unit test for r5py.util.snake_to_camel_case()
    
    * parametrised tests
    
    * debug statement left behind
    
    * remove print
    
    Co-authored-by: Christoph Fink <christoph@christophfink.com>
    Co-authored-by: Christoph Fink <1213882-christoph.fink@users.noreply.gitlab.com>
    Co-authored-by: Henrikki Tenkanen <henrikki.tenkanen@aalto.fi>

---
+++ tests/test_snake_to_camel_case.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+import pytest  # noqa: F401
+
+import r5py.util
+
+
+class TestSnakeToCamelCase:
+    @pytest.mark.parametrize(
+        ["snake_case", "camel_case"],
+        [
+            ("camel_case", "camelCase"),
+            ("get_http_response_code", "getHttpResponseCode"),
+            ("http_response_code_xyz", "httpResponseCodeXyz")
+        ]
+    )
+    def test_case_snake_case(self, snake_case, camel_case):
+        assert r5py.util.snake_to_camel_case(snake_case) == camel_case

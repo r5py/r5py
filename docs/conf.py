3abccef79743e30bd69c79fcc0be3af8aa0dd266
Henrikki Tenkanen <henrikki.tenkanen@aalto.fi>
2022-05-20 11:45:31 +0300

    Test binder from fork

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,9 @@ html_theme = 'sphinx_book_theme'
 
 html_theme_options = {
     # "external_links": [],
-    "repository_url": "https://github.com/r5py/r5py/",
+    "repository_url": "https://github.com/htenkanen/r5py/",
+    "repository_branch": "issue_34",
+    "path_to_docs": "docs",
     "use_edit_page_button": True,
     "launch_buttons": {
         "binderhub_url": "https://mybinder.org",

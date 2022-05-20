db82b7aeaa61545696e7b3ce1ee2f5a6dd485496
Henrikki Tenkanen <henrikki.tenkanen@aalto.fi>
2022-05-20 12:34:11 +0300

    Run binder from r5py main repository

--- docs/conf.py
+++ docs/conf.py
@@ -53,8 +53,8 @@ html_theme = 'sphinx_book_theme'
 
 html_theme_options = {
     # "external_links": [],
-    "repository_url": "https://github.com/htenkanen/r5py/",
-    "repository_branch": "issue_34",
+    "repository_url": "https://github.com/r5py/r5py/",
+    "repository_branch": "main",
     "path_to_docs": "docs",
     "use_edit_page_button": True,
     "launch_buttons": {

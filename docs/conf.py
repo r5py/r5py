b59ed26c09915e35de7b181749277d307d3e766f
Christoph Fink <christoph@christophfink.com>
2022-05-23 15:23:26 +0300

    testing the waters

--- docs/conf.py
+++ docs/conf.py
@@ -86,3 +86,8 @@ html_title = ""
 
 # Logo
 html_logo = "_static/r5py.png"
+
+# debugging: figure out whether RTD tells us which build (stable/latest) we are on:
+import json
+import os
+print(json.dumps(dict(os.environ), indent=4, sort_keys=True))

eb40c59afc13ccf35cd35c182eb06184b43ce647
Christoph Fink <1213882-christoph.fink@users.noreply.gitlab.com>
2022-08-16 11:22:58 +0300

    non-recursive test fixtures

--- tests/test_regional_task.py
+++ tests/test_regional_task.py
@@ -18,33 +18,25 @@ POPULATION_GRID_POINTS = DATA_DIRECTORY / "Helsinki" / "population_points_2020.g
 
 class TestRegionalTask:
     @pytest.fixture(scope="session")
-    def transport_network(self):
-        _transport_network = r5py.TransportNetwork(OSM_PBF, [GTFS])
-        yield _transport_network
-
-    @pytest.fixture(scope="session")
-    def grid_points(self):
-        _grid_points = geopandas.read_file(POPULATION_GRID_POINTS)
-        yield _grid_points
-
-    @pytest.fixture(scope="session")
-    def regional_task(self):
-        _regional_task = r5py.RegionalTask(
-            self.transport_network,
-            self.grid_points.at[1, "geometry"],
-            self.grid_points,
+    def blank_regional_task(self):
+        transport_network = r5py.TransportNetwork(OSM_PBF, [GTFS])
+        grid_points = geopandas.read_file(POPULATION_GRID_POINTS)
+        regional_task = r5py.RegionalTask(
+            transport_network,
+            grid_points.at[1, "geometry"],
+            grid_points,
         )
-        yield _regional_task
+        yield regional_task
 
     @pytest.mark.parametrize(
         ["regional_task", "percentiles"],
         [
-            (pytest.lazy_fixture("regional_task"), []),
-            (pytest.lazy_fixture("regional_task"), [50]),
-            (pytest.lazy_fixture("regional_task"), [33, 66]),
-            (pytest.lazy_fixture("regional_task"), [25, 50, 75]),
-            (pytest.lazy_fixture("regional_task"), [20, 40, 60, 80]),
-            (pytest.lazy_fixture("regional_task"), [16, 33, 50, 66, 83]),
+            (pytest.lazy_fixture("blank_regional_task"), []),
+            (pytest.lazy_fixture("blank_regional_task"), [50]),
+            (pytest.lazy_fixture("blank_regional_task"), [33, 66]),
+            (pytest.lazy_fixture("blank_regional_task"), [25, 50, 75]),
+            (pytest.lazy_fixture("blank_regional_task"), [20, 40, 60, 80]),
+            (pytest.lazy_fixture("blank_regional_task"), [16, 33, 50, 66, 83]),
         ],
     )
     def test_allowed_number_of_percentiles(self, regional_task, percentiles):
@@ -53,8 +45,14 @@ class TestRegionalTask:
     @pytest.mark.parametrize(
         ["regional_task", "percentiles"],
         [
-            (pytest.lazy_fixture("regional_task"), [10, 20, 30, 40, 50, 60, 70, 90]),
-            (pytest.lazy_fixture("regional_task"), [i for i in range(101)]),
+            (
+                pytest.lazy_fixture("blank_regional_task"),
+                [10, 20, 30, 40, 50, 60, 70, 90],
+            ),
+            (
+                pytest.lazy_fixture("blank_regional_task"),
+                [i for i in range(101)],
+            ),
         ],
     )
     def test_out_of_range_percentiles(self, regional_task, percentiles):

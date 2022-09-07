2680c554ac3851038b1f24f1ec6b186dbcfb9819
Christoph Fink <christoph.fink@gmail.com>
2022-09-07 14:55:40 +0200

    exceptions inheritence

--- src/r5py/util/exceptions.py
+++ src/r5py/util/exceptions.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 
-"""Location of custom errors."""
+"""R5py-specific exceptions."""
 
 
-class NoIDColumnError(Exception):
-    pass
+import requests
 
 
-class NonUniqueIDError(Exception):
-    pass
+class R5pyError(Exception):
+    """Generic base exception for r5py errors."""
+
+
+class ChecksumFailed(requests.RequestException, R5pyError):
+    """Requested resource did not pass checksum test."""
+
+
+class NonUniqueIDError(ValueError, R5pyError):
+    """An input data set’s `id` column has non-unique values."""
+
+
+class MissingColumnError(ValueError, R5pyError):
+    """An input data set is missing a required column."""
+
+
+class NoIDColumnError(MissingColumnError):
+    """An input data set does not have a required `id` column."""

e54c32b7b8f49c30c97a22c496cb5bba71d75232
Christoph Fink <christoph.fink@gmail.com>
2022-09-10 14:09:15 +0300

    copy/link files to working copy in cache path, clean afterwards

--- src/r5py/r5/transport_network.py
+++ src/r5py/r5/transport_network.py
@@ -5,13 +5,15 @@
 
 
 import json
+import functools
 import pathlib
+import shutil
 import warnings
 
 import jpype
 import jpype.types
 
-from ..util import contains_gtfs_data, start_jvm
+from ..util import config, contains_gtfs_data, start_jvm
 from .transport_network_builder_config import TransportNetworkBuilderConfig
 
 import com.conveyal.r5
@@ -42,16 +44,18 @@ class TransportNetwork:
             options accepted by TNBuilderConfig (including SpeedConfig)
         """
         # TODO: Add TNBuilderConfig and SpeedConfig options to docstring
-        osm_pbf = pathlib.Path(osm_pbf).absolute()
+        osm_pbf = self._working_copy(pathlib.Path(osm_pbf)).absolute()
 
-        gtfs = [str(pathlib.Path(path).absolute()) for path in gtfs]
+        gtfs = [str(self._working_copy(path).absolute()) for path in gtfs]
         build_config = TransportNetworkBuilderConfig(**build_config)
         self._transport_network = com.conveyal.r5.transit.TransportNetwork.fromFiles(
             java.lang.String(str(osm_pbf)), java.util.ArrayList.of(gtfs), build_config
         )
         self._transport_network.transitLayer.buildDistanceTables(None)
 
-        # remove temporary files created by R5 during import
+        # remove temporary files created by R5 during import.
+        # this can be done immediately, they’re not needed anymore,
+        # and potentially take up temp/cache space
         for temp_file in osm_pbf.parent.glob(f"{osm_pbf.name}.mapdb*"):
             temp_file.unlink()
 
@@ -107,6 +111,10 @@ class TransportNetwork:
 
         return cls(osm_pbf, gtfs, build_json)
 
+    def __del__(self):
+        """Clean cache directory when done."""
+        shutil.rmtree(str(self._cache_directory))
+
     def __enter__(self):
         """Provide a context."""
         return self
@@ -114,6 +122,46 @@ class TransportNetwork:
     def __exit__(self, exception_type, exception_value, traceback):
         """Exit context."""
         return False
+
+    @functools.cached_property
+    def _cache_directory(self):
+        cache_dir = (
+            pathlib.Path(config.CACHE_DIR) / f"{self.__class__.__name__}_{hash(self)}"
+        )
+        cache_dir.mkdir(exist_ok=True)
+        return cache_dir
+
+    def _working_copy(self, input_file):
+        """Create a copy or link of an input file in a cache directory.
+
+        This method exists because R5 creates temporary files in the
+        directory of input files. This can not only be annoying clutter,
+        but also create problems of concurrency, performance, etc., for
+        instance, when the data comes from a shared network drive or a
+        read-only file system.
+
+        Arguments
+        ---------
+        input_file : str or pathlib.Path
+            The file to create a copy or link of in a cache directory
+
+        Returns
+        -------
+        pathlib.Path
+            The path to the copy or link created
+        """
+        # try to first create a symbolic link, if that fails (e.g., on Windows),
+        # copy the file to a cache directory
+        input_file = pathlib.Path(input_file)
+        destination_file = pathlib.Path(self._cache_directory / input_file.name)
+        try:
+            destination_file.symlink_to(input_file)
+        except OSError:
+            shutil.copyfile(
+                str(input_file),
+                str(destination_file)
+            )
+        return destination_file
 
     @property
     def linkage_cache(self):

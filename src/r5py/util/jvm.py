feeec375706fae7699853d26cfb7bfb9335c1a4c
Christoph Fink <christoph.fink@gmail.com>
2022-09-28 12:40:07 +0300

    libjsig.so is (also) in same directory as libjvm.so

--- src/r5py/util/jvm.py
+++ src/r5py/util/jvm.py
@@ -30,7 +30,7 @@ def start_jvm():
         # (cf. https://stackoverflow.com/questions/
         #   15790403/what-does-consider-using-jsig-library-mean )
         os.environ["LD_PRELOAD"] = str(
-            pathlib.Path(jpype.getDefaultJVMPath()).parent.parent / "libjsig.so"
+            pathlib.Path(jpype.getDefaultJVMPath()).parent / "libjsig.so"
         )
         jpype.startJVM(
             f"-Xmx{MAX_JVM_MEMORY:d}",

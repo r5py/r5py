3bb6efaeecb259affb4937b653bc7b6870676a1c
Christoph Fink <christoph.fink@gmail.com>
2022-10-04 16:30:22 +0300

    more access modes, departure

--- tests/test_regional_task.py
+++ tests/test_regional_task.py
@@ -3,10 +3,13 @@
 
 import pytest  # noqa: F401
 
+import datetime
+
 import r5py
 from r5py.util import start_jvm
 
 import com.conveyal.r5
+import java.time
 
 
 start_jvm()
@@ -22,6 +25,26 @@ class TestRegionalTask:
                 [r5py.LegMode.WALK],
                 set([r5py.LegMode.WALK]),
             ),
+            (
+                pytest.lazy_fixture("blank_regional_task"),
+                [r5py.LegMode.BICYCLE],
+                set([r5py.LegMode.BICYCLE]),
+            ),
+            (
+                pytest.lazy_fixture("blank_regional_task"),
+                [r5py.LegMode.CAR],
+                set([r5py.LegMode.CAR]),
+            ),
+            (
+                pytest.lazy_fixture("blank_regional_task"),
+                [r5py.LegMode.WALK, r5py.LegMode.BICYCLE],
+                set([r5py.LegMode.WALK, r5py.LegMode.BICYCLE]),
+            ),
+            (
+                pytest.lazy_fixture("blank_regional_task"),
+                [r5py.LegMode.WALK, r5py.LegMode.BICYCLE, r5py.LegMode.CAR],
+                set([r5py.LegMode.WALK, r5py.LegMode.BICYCLE, r5py.LegMode.CAR]),
+            ),
         ],
     )
     def test_access_mode_setter(self, regional_task, access_modes, expected):
@@ -30,6 +53,24 @@ class TestRegionalTask:
         assert regional_task._regional_task.accessModes == r5py.RegionalTask._enum_set(
             expected, com.conveyal.r5.api.util.LegMode
         )
+
+    @pytest.mark.parametrize(
+        ["regional_task", "departure", "expected", "expected_java_date", "expected_java_from_time"],
+        [
+            (
+                pytest.lazy_fixture("blank_regional_task"),
+                datetime.datetime(2022, 2, 22, 8, 30),
+                datetime.datetime(2022, 2, 22, 8, 30),
+                java.time.LocalDate.of(2022, 2, 22),
+                30600
+            )
+        ]
+    )
+    def test_departure_setter(self, regional_task, departure, expected, expected_java_date, expected_java_from_time):
+        regional_task.departure = departure
+        assert regional_task.departure == expected
+        assert regional_task._regional_task.date == expected_java_date
+        assert regional_task._regional_task.fromTime == expected_java_from_time
 
     @pytest.mark.parametrize(
         ["regional_task", "percentiles"],

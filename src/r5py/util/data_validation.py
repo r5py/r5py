eb72db1b3e2d97044444602b282de65ac2a91bf9
Christoph Fink <christoph.fink@gmail.com>
2022-10-06 10:37:59 +0300

    fix typo in error message

--- src/r5py/util/data_validation.py
+++ src/r5py/util/data_validation.py
@@ -12,7 +12,7 @@ __all__ = ["check_od_data_set"]
 def check_od_data_set(od_data_set):
     """Check whether an origin/destination data set fulfils certain minimum criteria.
 
-    Checks whether `od_data_set` has an `id` column with unique values, and a cartographic
+    Checks whether `od_data_set` has an `id` column with unique values, and a coordinate
     reference system defined. Raises a `r5py.util.exceptions.NoIDColumnError`, a
     `r5py.util.exceptions.NonUniqueIDError`, or a `r5py.util.exceptions.NoCrsError`
     otherwise.
@@ -28,5 +28,5 @@ def check_od_data_set(od_data_set):
         raise NonUniqueIDError("Id values must be unique.")
     if od_data_set.crs is None:
         raise NoCrsError(
-            "Data set has to have a cartographic reference system defined."
+            "Data set has to have a coordinate reference system defined."
         )

fb3e3584e46213bfe70d25002f3d93fe787410fe
Christoph Fink <christoph.fink@gmail.com>
2022-11-30 19:48:30 +0200

    check whether departure in gtfs time range

--- src/r5py/r5/transit_layer.py
+++ src/r5py/r5/transit_layer.py
@@ -34,12 +34,15 @@ class TransitLayer:
         try:
             self._start_date
         except AttributeError:
-            self._start_date = min(
-                [
-                    parse_int_date(service.calendar.start_date)
-                    for service in self._transit_layer.services
-                ]
-            )
+            try:
+                self._start_date = min(
+                    [
+                        parse_int_date(service.calendar.start_date)
+                        for service in self._transit_layer.services
+                    ]
+                )
+            except ValueError as exception:
+                raise ValueError("No GTFS data set loaded") from exception
 
     @property
     def end_date(self):
@@ -47,12 +50,27 @@ class TransitLayer:
         try:
             self._end_date
         except AttributeError:
-            self._end_date = max(
-                [
-                    parse_int_date(service.calendar.end_date)
-                    for service in self._transit_layer.services
-                ]
-            ) + datetime.timedelta(hours=23, minutes=59, seconds=59)
+            try:
+                end_date = max(
+                    [
+                        parse_int_date(service.calendar.end_date)
+                        for service in self._transit_layer.services
+                    ]
+                )
+                end_date += datetime.timedelta(
+                    hours=23, minutes=59, seconds=59
+                )  # *end* of day
+                self._end_date = end_date
+            except ValueError as exception:
+                raise ValueError("No GTFS data set loaded") from exception
+
+    def covers(self, point_in_time):
+        """Check whether `point_in_time` is covered by GTFS data sets."""
+        try:
+            covers = self.start_date <= point_in_time <= self.end_date
+        except ValueError:  # no GTFS data loaded
+            covers = False
+        return covers
 
 
 @jpype._jcustomizer.JConversion(

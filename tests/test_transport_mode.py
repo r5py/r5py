f0262db360f3b27fedffe8cfb20e1e2947aad5fc
Christoph Fink <christoph.fink@gmail.com>
2023-04-20 13:22:43 +0300

    Check that TransportMode rejects invalid values, make case-insensitive

--- tests/test_transport_mode.py
+++ tests/test_transport_mode.py
@@ -53,3 +53,68 @@ class TestTransportMode:
     def test_transportmode(self, enum_member, enum_name, java_type):
         assert enum_member.name == enum_name
         assert enum_member.name == java_type.valueOf(enum_name).name()
+
+    @pytest.mark.parametrize(
+        ["str_transport_mode", "expected_transport_mode"],
+        [
+            ("AIR", r5py.TransportMode.AIR),
+            ("BUS", r5py.TransportMode.BUS),
+            ("CABLE_CAR", r5py.TransportMode.CABLE_CAR),
+            ("FERRY", r5py.TransportMode.FERRY),
+            ("FUNICULAR", r5py.TransportMode.FUNICULAR),
+            ("GONDOLA", r5py.TransportMode.GONDOLA),
+            ("RAIL", r5py.TransportMode.RAIL),
+            ("SUBWAY", r5py.TransportMode.SUBWAY),
+            ("TRAM", r5py.TransportMode.TRAM),
+            ("TRANSIT", r5py.TransportMode.TRANSIT),
+            ("BICYCLE", r5py.TransportMode.BICYCLE),
+            ("CAR", r5py.TransportMode.CAR),
+            ("WALK", r5py.TransportMode.WALK),
+            ("BICYCLE_RENT", r5py.TransportMode.BICYCLE_RENT),
+            ("CAR_PARK", r5py.TransportMode.CAR_PARK),
+
+            ("air", r5py.TransportMode.AIR),
+            ("bus", r5py.TransportMode.BUS),
+            ("cable_car", r5py.TransportMode.CABLE_CAR),
+            ("ferry", r5py.TransportMode.FERRY),
+            ("funicular", r5py.TransportMode.FUNICULAR),
+            ("gondola", r5py.TransportMode.GONDOLA),
+            ("rail", r5py.TransportMode.RAIL),
+            ("subway", r5py.TransportMode.SUBWAY),
+            ("tram", r5py.TransportMode.TRAM),
+            ("transit", r5py.TransportMode.TRANSIT),
+            ("bicycle", r5py.TransportMode.BICYCLE),
+            ("car", r5py.TransportMode.CAR),
+            ("walk", r5py.TransportMode.WALK),
+            ("bicycle_rent", r5py.TransportMode.BICYCLE_RENT),
+            ("car_park", r5py.TransportMode.CAR_PARK),
+
+            ("AiR", r5py.TransportMode.AIR),
+            ("bUs", r5py.TransportMode.BUS),
+            ("CaBlE_CaR", r5py.TransportMode.CABLE_CAR),
+            ("fErRy", r5py.TransportMode.FERRY),
+            ("FuNiCuLaR", r5py.TransportMode.FUNICULAR),
+            ("gOnDoLa", r5py.TransportMode.GONDOLA),
+            ("RaIl", r5py.TransportMode.RAIL),
+            ("SuBwAy", r5py.TransportMode.SUBWAY),
+            ("TrAm", r5py.TransportMode.TRAM),
+            ("TrAnSiT", r5py.TransportMode.TRANSIT),
+            ("bIcYcLe", r5py.TransportMode.BICYCLE),
+            ("CaR", r5py.TransportMode.CAR),
+            ("wAlK", r5py.TransportMode.WALK),
+            ("bIcYcLe_ReNt", r5py.TransportMode.BICYCLE_RENT),
+            ("CaR_PaRk", r5py.TransportMode.CAR_PARK),
+        ]
+    )
+    def test_str_transportmode(self, str_transport_mode, expected_transport_mode):
+        assert r5py.TransportMode(str_transport_mode) == expected_transport_mode
+
+    @pytest.mark.parametrize(
+        ["invalid_transport_mode"],
+        [
+            ("Helicopter",), ("adsffoobar",), (13,), (None,), ("1234",),
+        ]
+    )
+    def test_invalid_transportmode(self, invalid_transport_mode):
+        with pytest.raises(ValueError, match="is not a valid TransportMode"):
+            _ = r5py.TransportMode(invalid_transport_mode)

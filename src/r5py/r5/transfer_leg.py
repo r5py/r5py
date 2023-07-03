95a14d10b6a8010150d3c83c4f6c7703681abf2e
Christoph Fink <christoph.fink@gmail.com>
2023-07-03 13:46:27 +0300

    refactored TransferLeg

--- src/r5py/r5/transfer_leg.py
+++ src/r5py/r5/transfer_leg.py
@@ -16,22 +16,3 @@ class TransferLeg(DirectLeg):
     Represent one leg of a trip, specifically transfers between public transport
     vehicles.
     """
-
-    def __init__(self, transport_mode, street_segment, travel_time=None):
-        """
-        Represent one leg of a trip, specifically transfers between public
-        transport vehicles.
-
-        Arguments
-        =========
-        transport_mode : r5py.TransportMode
-            mode of transport this trip leg was travelled
-        street_segment : com.conveyal.r5.profile.StreetSegment
-            the leg’s data as output by R5’s `StreetRouter`
-        travel_time : datetime.timedelta
-            travel time for this leg, if not specified (default),
-            use `street_segment.duration`
-        """
-        super().__init__(transport_mode, street_segment)
-        if travel_time is not None:
-            self.travel_time = travel_time

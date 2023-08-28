7ebe4888bbedd72a4e41319c5653980aa796537d
Christoph Fink <christoph.fink@gmail.com>
2023-08-28 16:47:27 +0300

    SampleDataSet, download in __getattr__

--- src/r5py/util/data_set.py
+++ src/r5py/util/data_set.py
@@ -4,64 +4,16 @@
 """A remote data set that is downloaded on demand."""
 
 
-import hashlib
-import pathlib
 import warnings
 
-from r5py.util.config import Config
-from r5py.util.validating_requests_session import ValidatingRequestsSession
+from .sample_data_set import SampleDataSet
 
 
-config = Config()
-
-
-class DataSet(pathlib.Path):
-    """Data set that is downloaded and cached as needed."""
-
-    # decide which kind of pathlib.Path we are (Windows, Unix, ...)
-    # cf. https://stackoverflow.com/a/66613346/463864
-    _flavour = type(pathlib.Path())._flavour
-
-    _CACHE_DIR = pathlib.Path(config.CACHE_DIR) / "sampledata"
-
-    def __new__(cls, remote_url, sha256_checksum):
-        # pathlib.Path does everything in __new__, rather than __init__
-        cached_path = cls._CACHE_DIR / pathlib.Path(remote_url).name
-        return super().__new__(cls, cached_path)
-
-    def __init__(self, remote_url, sha256_checksum, *args, **kwargs):
-        """
-        Define a data set that is downloaded and cached on demand.
-
-        Arguments
-        ---------
-        remote_url : str
-            source URL for this data set
-        sha256_checksum : str
-            checksum for this data set, using an SHA256 algorithm
-        """
-        super().__init__()
-        self.remote_url = remote_url
-        self.checksum = sha256_checksum
-        self.cached_path = (
-            pathlib.Path(config.CACHE_DIR) / pathlib.Path(remote_url).name
+class DataSet(SampleDataSet):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        warnings.warn(
+            "r5py.util.data_set.DataSet() is deprecated, use"
+            "r5py.util.sample_data_set.SampleDataSet, instead.",
+            DeprecationWarning
         )
-        self._download_remote_file()
-
-    def _download_remote_file(self):
-        try:
-            assert (
-                hashlib.sha256(self.cached_path.read_bytes()).hexdigest()
-                == self.checksum
-            )
-        except (AssertionError, FileNotFoundError):
-            if config.arguments.verbose:
-                warnings.warn(
-                    f"First access to {pathlib.Path(self.remote_url).name}, "
-                    "downloading remote file to local cache",
-                    RuntimeWarning,
-                )
-            with ValidatingRequestsSession() as session, session.get(
-                self.remote_url, self.checksum
-            ) as response:
-                self.cached_path.write_bytes(response.content)

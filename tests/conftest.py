0bbbfaf59069612260127bb31ef2e5f22667f109
Christoph Fink <christoph.fink@gmail.com>
2024-01-10 09:51:52 +0200

    added another small patch to our own r5, changed max_path logic

--- tests/conftest.py
+++ tests/conftest.py
@@ -33,7 +33,7 @@ SINGLE_VALID_ORIGIN = DATA_DIRECTORY / "test_valid_single_point_data.geojson"
 R5_JAR_URL = (
     "https://github.com/r5py/r5/releases/download/v7.1-r5py/r5-v7.1-r5py-all.jar"
 )
-R5_JAR_SHA256 = "fde9683678b51588393a9316c5d6878eb0a58f17372e496dd188861c30e78371"
+R5_JAR_SHA256 = "cd697b50323fd99977c98039ea317698bcf5fbbdb12b59e3e094ae9443648db2"
 R5_JAR_SHA256_INVALID = "adfadsfadsfadsfasdfasdf"
 R5_JAR_SHA256_GITHUB_ERROR_MESSAGE_WHEN_POSTING = (
     "14aa2347be79c280e4d0fd3a137fb8f5bf2863261a1e48e1a122df1a52a0f453"

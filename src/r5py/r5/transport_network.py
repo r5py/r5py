4b6e8177cbef7c8505b7b8c26a368e748dc6012a
Christoph Fink <christoph+github@christophfink.com>
2024-01-15 13:54:00 +0200

    Better catch possible exceptions in TransportNetwork.__del__() (#391)
    * Better catch possible exceptions in TransportNetwork.__del__()
    
    * updated codecov action version

--- src/r5py/r5/transport_network.py
+++ src/r5py/r5/transport_network.py
@@ -120,10 +120,16 @@ class TransportNetwork:
             del self.transit_layer
         except AttributeError:
             pass
-        del self._transport_network
+        try:
+            del self._transport_network
+        except AttributeError:
+            pass
 
         time.sleep(0.5)
-        jpype.java.lang.System.gc()
+        try:
+            jpype.java.lang.System.gc()
+        except jpype.JVMNotRunning:
+            pass
 
         # then, try to delete all files in cache directory
         temporary_files = [child for child in self._cache_directory.iterdir()]

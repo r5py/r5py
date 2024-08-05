87e44b73fdf5b20a39c2ed660c232a27d624312d
Christoph Fink <christoph+github@christophfink.com>
2024-08-05 15:58:28 +0200

    Update to R5 v7.2 (#432)
    Co-authored-by: Christoph Fink <christoph.fink@christophfink.com>

--- tests/conftest_d/r5_jar.py
+++ tests/conftest_d/r5_jar.py
@@ -10,9 +10,9 @@ import pytest
 
 
 R5_JAR_URL = (
-    "https://github.com/r5py/r5/releases/download/v7.1-r5py/r5-v7.1-r5py-all.jar"
+    "https://github.com/r5py/r5/releases/download/v7.2-r5py/r5-v7.2-r5py-all.jar"
 )
-R5_JAR_SHA256 = "cd697b50323fd99977c98039ea317698bcf5fbbdb12b59e3e094ae9443648db2"
+R5_JAR_SHA256 = "f693af105b4b9c5fb317f0c81cf2d6d54a46b3b56a7c4817454d758920fbe706"
 R5_JAR_SHA256_INVALID = "adfadsfadsfadsfasdfasdf"
 R5_JAR_SHA256_GITHUB_ERROR_MESSAGE_WHEN_POSTING = (
     "14aa2347be79c280e4d0fd3a137fb8f5bf2863261a1e48e1a122df1a52a0f453"

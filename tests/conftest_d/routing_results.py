9c7ff445e84821c24945391b98893ee6367ff3bc
Christoph Fink <christoph.fink@gmail.com>
2025-01-28 15:53:02 +0100

    test ttm with different transport modes

--- tests/conftest_d/routing_results.py
+++ tests/conftest_d/routing_results.py
@@ -19,6 +19,16 @@ DETAILED_ITINERARIES_TRANSIT = (
     DATA_DIRECTORY / "test_detailed_itineraries_transit.gpkg.zip"
 )
 DETAILED_ITINERARIES_WALK = DATA_DIRECTORY / "test_detailed_itineraries_walk.gpkg.zip"
+
+TRAVEL_TIMES_BICYCLE = (
+    DATA_DIRECTORY / "test_travel_times_bicycle.csv"
+)
+TRAVEL_TIMES_CAR = DATA_DIRECTORY / "test_travel_times_car.csv"
+TRAVEL_TIMES_TRANSIT = (
+    DATA_DIRECTORY / "test_travel_times_transit.csv"
+)
+TRAVEL_TIMES_WALK = DATA_DIRECTORY / "test_travel_times_walk.csv"
+
 WALKING_DETAILS_NOT_SNAPPED = DATA_DIRECTORY / "test_walking_details_not_snapped.csv"
 WALKING_DETAILS_SNAPPED = DATA_DIRECTORY / "test_walking_details_snapped.csv"
 WALKING_TIMES_NOT_SNAPPED = DATA_DIRECTORY / "test_walking_times_not_snapped.csv"
@@ -47,6 +57,30 @@ def detailed_itineraries_transit():
 def detailed_itineraries_walk():
     """Retrieve expected detailed itineraries for walking."""
     yield geopandas.read_file(DETAILED_ITINERARIES_WALK)
+
+
+@pytest.fixture
+def travel_times_bicycle():
+    """Retrieve expected travel times for cycling."""
+    yield pandas.read_csv(TRAVEL_TIMES_BICYCLE)
+
+
+@pytest.fixture
+def travel_times_car():
+    """Retrieve expected travel times for driving."""
+    yield pandas.read_csv(TRAVEL_TIMES_CAR)
+
+
+@pytest.fixture
+def travel_times_transit():
+    """Retrieve expected travel times for public transport."""
+    yield pandas.read_csv(TRAVEL_TIMES_TRANSIT)
+
+
+@pytest.fixture
+def travel_times_walk():
+    """Retrieve expected travel times for walking."""
+    yield pandas.read_csv(TRAVEL_TIMES_WALK)
 
 
 @pytest.fixture

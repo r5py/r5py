602df200d67b5d810d26996582d03ed9d31026d4
Christoph Fink <christoph.fink@gmail.com>
2025-02-18 15:11:19 +0100

    test binary file so Windows cannot tamper with linefeeds

--- tests/conftest_d/file_digest.py
+++ tests/conftest_d/file_digest.py
@@ -6,22 +6,22 @@
 
 import pytest
 
-from .routing_results import TRAVEL_TIMES_WALK
+from .routing_results import ISOCHRONES_WALK
 
 
-TRAVEL_TIMES_WALK_SHA256 = (
-    "fefe71155352c8daddfa84ea3f1bcc6c17dd7f358b1a17d0c3e361d6c8a9ada6"
+ISOCHRONES_WALK_SHA256 = (
+    "4ccdb2c48c55054d2c6f5de60d47eaf50df8e637ff2e9836111a9519e7450d85"
 )
-TRAVEL_TIMES_WALK_BLAKE2B = "a4227c5dcd2ad7275b6bd240a8cdd9fecdedb0677e6b9b0232fd7af48695741a6667c47ff2cf27add7b17139976e235c569e72efe65d5e363b8b5f37e4bdd0fe"
-TRAVEL_TIMES_WALK_BLAKE2S = (
-    "fa48e033de6894948c23405134fe4a1c708ce2aa126f68f9b45e3bec0fd4b99d"
+ISOCHRONES_WALK_BLAKE2B = "72c129837f757d72a214c57cbb9545d3bd2ecc499053a5d941b012a479a1a7739b9622dbef7bb93c3f1ba95834bf84aed0aa73257956209ba1237bc690fc6ce6"
+ISOCHRONES_WALK_BLAKE2S = (
+    "e01d5543a03eb458b97959d4f1ea9b28c5d1e765751a5ae347a6b43df7cfdc51"
 )
 
 
 @pytest.fixture
 def file_digest_test_file_as_pathlib_path():
     """Return the path of a test file as a pathlib.Path."""
-    yield TRAVEL_TIMES_WALK
+    yield ISOCHRONES_WALK
 
 
 @pytest.fixture
@@ -33,16 +33,16 @@ def file_digest_test_file_as_str(file_digest_test_file_as_pathlib_path):
 @pytest.fixture()
 def file_digest_sha256():
     """Return the expected SHA256 hash for the test file."""
-    yield TRAVEL_TIMES_WALK_SHA256
+    yield ISOCHRONES_WALK_SHA256
 
 
 @pytest.fixture()
 def file_digest_blake2b():
     """Return the expected BLAKE2B hash for the test file."""
-    yield TRAVEL_TIMES_WALK_BLAKE2B
+    yield ISOCHRONES_WALK_BLAKE2B
 
 
 @pytest.fixture()
 def file_digest_blake2s():
     """Return the expected BLAKE2S hash for the test file."""
-    yield TRAVEL_TIMES_WALK_BLAKE2S
+    yield ISOCHRONES_WALK_BLAKE2S

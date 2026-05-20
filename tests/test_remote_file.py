3bd3696f789c204e290190d0df02e0ddc3fe10a4
Christoph Fink <christoph.fink@christophfink.com>
2026-05-20 15:19:44 +0200

    tests

---
+++ tests/test_remote_file.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+
+import pathlib
+import sys
+
+import pytest
+
+from r5py.util.config import Config
+from r5py.util.remote_file import RemoteFile
+
+
+class TestRemoteFile:
+    def test_data_set_with_verbose(self, sample_data_set_url, sample_data_set_sha256):
+        sys.argv.extend(
+            [
+                "--verbose",
+            ]
+        )
+
+        # make sure the cached file does not exist
+        try:
+            (
+                pathlib.Path(Config().CACHE_DIR)
+                / "remote"
+                / pathlib.Path(sample_data_set_url).name
+            ).unlink()
+        except FileNotFoundError:
+            pass
+
+        with pytest.warns(
+            RuntimeWarning,
+            match="First access to .*, downloading remote file to local cache",
+        ):
+            data_set = RemoteFile(sample_data_set_url, sample_data_set_sha256)
+            assert data_set.exists()
+
+        data_set.unlink()
+        sys.argv = sys.argv[:-1]
+
+    def test_data_set_with_verbose_without_checksum(self, sample_data_set_url):
+        sys.argv.extend(
+            [
+                "--verbose",
+            ]
+        )
+
+        # make sure the cached file does not exist
+        try:
+            (
+                pathlib.Path(Config().CACHE_DIR)
+                / "remote"
+                / pathlib.Path(sample_data_set_url).name
+            ).unlink()
+        except FileNotFoundError:
+            pass
+
+        with pytest.warns(
+            RuntimeWarning,
+            match="First access to .*, downloading remote file to local cache",
+        ):
+            data_set = RemoteFile(sample_data_set_url)
+            assert data_set.exists()
+
+        data_set.unlink()
+        sys.argv = sys.argv[:-1]
+
+    def test_data_set_with_verbose_with_corruption(
+        self, sample_data_set_url, sample_data_set_sha256
+    ):
+        sys.argv.extend(
+            [
+                "--verbose",
+            ]
+        )
+
+        # make sure the cached file does not exist
+        (pathlib.Path(Config().CACHE_DIR) / "remote").mkdir(parents=True, exist_ok=True)
+        (
+            pathlib.Path(Config().CACHE_DIR)
+            / "remote"
+            / pathlib.Path(sample_data_set_url).name
+        ).write_text("CORRUPTED CONTENT OF SAMPLE DATA SET")
+
+        with pytest.warns(
+            RuntimeWarning,
+            match=".* changed, re-downloading remote file to local cache",
+        ):
+            data_set = RemoteFile(sample_data_set_url, sample_data_set_sha256)
+            assert data_set.exists()
+
+        data_set.unlink()
+        sys.argv = sys.argv[:-1]
+
+    def test_data_set(self, sample_data_set_url, sample_data_set_sha256):
+        data_set = RemoteFile(sample_data_set_url, sample_data_set_sha256)
+        assert data_set.exists()
+        data_set.unlink()
+
+    def test_data_set_invalid_hash(self, sample_data_set_url, sample_data_set_sha256):
+        data_set = RemoteFile(sample_data_set_url, sample_data_set_sha256)
+        data_set.write_text("foobar")  # change file, invalidate checksum
+        del data_set
+
+        # try again:
+        data_set = RemoteFile(sample_data_set_url, sample_data_set_sha256)
+        assert data_set.exists()
+        data_set.unlink()
---
+++ tests/test_working_copy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+import pathlib
+
+from r5py.util.config import Config
+from r5py.util.sample_data_set import SampleDataSet
+from r5py.util.working_copy import WorkingCopy
+
+
+class TestWorkingCopy:
+    def test_working_copy_with_https_url(self, sample_data_set_url):
+        working_copy = WorkingCopy(sample_data_set_url)
+        assert pathlib.Path(Config().CACHE_DIR / working_copy.name).exists()
+
+    def test_working_copy_with_file_url(self, sample_data_set_url, sample_data_set_sha256):
+        sample_data_set = SampleDataSet(sample_data_set_url, sample_data_set_sha256)
+        working_copy = WorkingCopy(f"file://{sample_data_set}")
+        assert pathlib.Path(Config().CACHE_DIR / working_copy.name).exists()

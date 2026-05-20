7c8426299aabb217ed01ab4f0072bd12f42581de
Christoph Fink <christoph.fink@christophfink.com>
2026-05-20 15:20:08 +0200

    linted

--- tests/test_working_copy.py
+++ tests/test_working_copy.py
@@ -13,7 +13,9 @@ class TestWorkingCopy:
         working_copy = WorkingCopy(sample_data_set_url)
         assert pathlib.Path(Config().CACHE_DIR / working_copy.name).exists()
 
-    def test_working_copy_with_file_url(self, sample_data_set_url, sample_data_set_sha256):
+    def test_working_copy_with_file_url(
+        self, sample_data_set_url, sample_data_set_sha256
+    ):
         sample_data_set = SampleDataSet(sample_data_set_url, sample_data_set_sha256)
         working_copy = WorkingCopy(f"file://{sample_data_set}")
         assert pathlib.Path(Config().CACHE_DIR / working_copy.name).exists()

fe127111c48e1cc43af74fb4d12d1f1a604f352a
Christoph Fink <christoph.fink@gmail.com>
2026-09-17 20:24:19 +0200

    create new working copies every time

--- tests/test_working_copy.py
+++ tests/test_working_copy.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 
+import filecmp
 import pathlib
+import random
+
+import pytest
+
+from .temporary_directory import TemporaryDirectory
 
 from r5py.util.config import Config
 from r5py.util.sample_data_set import SampleDataSet
@@ -19,3 +25,50 @@ class TestWorkingCopy:
         sample_data_set = SampleDataSet(sample_data_set_url, sample_data_set_sha256)
         working_copy = WorkingCopy(f"file://{sample_data_set}")
         assert pathlib.Path(Config().CACHE_DIR / working_copy.name).exists()
+
+    @pytest.mark.parametrize(
+        ["input_file_text_a", "input_file_text_b"],
+        [("foo", "bar")],
+    )
+    def test_working_copy_change_source_file(
+        self,
+        input_file_text_a,
+        input_file_text_b,
+    ):
+        with TemporaryDirectory() as temp_directory:
+            input_file = pathlib.Path(temp_directory) / "test_input.txt"
+
+            input_file.write_text(input_file_text_a)
+            working_copy_a = WorkingCopy(input_file)
+            assert working_copy_a.read_text() == input_file_text_a
+
+            input_file.write_text(input_file_text_b)
+            working_copy_b = WorkingCopy(input_file)
+            assert working_copy_b.read_text() == input_file_text_b
+
+    def test_working_copy_from_temp_directory(self):
+        with TemporaryDirectory() as temp_directory:
+            input_file = pathlib.Path(temp_directory) / "test_input.txt"
+            input_file.write_text(f"{random.randrange(16**5):032x}")
+
+            working_copy = WorkingCopy(input_file)
+
+            assert filecmp.cmp(input_file, working_copy, shallow=False)
+
+            working_copy.unlink()
+
+    def test_working_copy_failed_symlink(self, monkeypatch):
+        def _symlink_to(*args, **kwargs):
+            raise OSError
+
+        monkeypatch.setattr(pathlib.Path, "symlink_to", _symlink_to)
+
+        with TemporaryDirectory() as temp_directory:
+            input_file = pathlib.Path(temp_directory) / "test_input.txt"
+            input_file.write_text(f"{random.randrange(16**5):032x}")
+
+            working_copy = WorkingCopy(input_file)
+
+            assert filecmp.cmp(input_file, working_copy, shallow=False)
+
+            working_copy.unlink()
